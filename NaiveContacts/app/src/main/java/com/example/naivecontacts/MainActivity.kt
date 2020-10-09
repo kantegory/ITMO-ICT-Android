a191dc8d07b8ee093e73720169e69747901ed376
kantegory <davidobryakov@gmail.com>
2020-10-09 22:55:35 +0300

    [easy] Add READ_CONTACTS permission request

--- NaiveContacts/app/src/main/java/com/example/naivecontacts/MainActivity.kt
+++ NaiveContacts/app/src/main/java/com/example/naivecontacts/MainActivity.kt
@@ -1,24 +1,69 @@
 package com.example.naivecontacts
 
 import Contact
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import fetchAllContacts
 
+
 class MainActivity : AppCompatActivity() {
+
+    private var REQUEST_CODE_PERMISSION_READ_CONTACTS : Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val contactsList: RecyclerView = findViewById(R.id.recyclerViewContacts)
-        val fetchedContacts : List<Contact> = fetchAllContacts()
-        val contactsAdapter: ContactsAdapter = ContactsAdapter(fetchedContacts.size, fetchedContacts)
+        val permissionStatus =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
 
-        val layoutManager = LinearLayoutManager(this)
-        contactsList.layoutManager = layoutManager
-        contactsList.adapter = contactsAdapter
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            val contactsList: RecyclerView = findViewById(R.id.recyclerViewContacts)
+            val fetchedContacts : List<Contact> = fetchAllContacts()
+            val contactsAdapter: ContactsAdapter = ContactsAdapter(fetchedContacts.size, fetchedContacts)
+
+            val layoutManager = LinearLayoutManager(this)
+            contactsList.layoutManager = layoutManager
+            contactsList.adapter = contactsAdapter
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.READ_CONTACTS),
+                REQUEST_CODE_PERMISSION_READ_CONTACTS
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_PERMISSION_READ_CONTACTS -> {
+                if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    // permission granted
+                    val contactsList: RecyclerView = findViewById(R.id.recyclerViewContacts)
+                    val fetchedContacts : List<Contact> = fetchAllContacts()
+                    val contactsAdapter: ContactsAdapter = ContactsAdapter(fetchedContacts.size, fetchedContacts)
+
+                    val layoutManager = LinearLayoutManager(this)
+                    contactsList.layoutManager = layoutManager
+                    contactsList.adapter = contactsAdapter
+                } else {
+                    // permission denied
+                    Toast.makeText(applicationContext,"К сожалению, Вы не дали доступа к контактам", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
     }
 }
