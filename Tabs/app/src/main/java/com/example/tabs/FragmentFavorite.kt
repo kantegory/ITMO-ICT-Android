a4c162d34c4415e20f7cffda708e49d018e1c031
kantegory <davidobryakov@gmail.com>
2020-10-23 21:20:32 +0300

    [easy] Add FragmentFavorite files

---
+++ Tabs/app/src/main/java/com/example/tabs/FragmentFavorite.kt
@@ -0,0 +1,14 @@
+package com.example.tabs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentFavorite : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        inflater.inflate(R.layout.fragment_favorite, container, false)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
