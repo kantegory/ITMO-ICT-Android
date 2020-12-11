da60470f58b0bbeb8d78bb559d2f4cbfb3dbf4e8
David Dobryakov <davidobryakov@gmail.com>
2020-12-11 16:48:43 +0300

    [easy] Implement RecyclerView for FBT items

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/FBTAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.weatherapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.TextView
+
+class FBTAdapter (private val FBTNumber: Int, private val FBTList: List<FBT>)
+    : RecyclerView.Adapter<FBTAdapter.FBTViewHolder>()  {
+        class FBTViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView)  {
+            // получаем доступ ко всем элементам внутри нашей карточки
+            private val FBTTime : TextView = itemView.findViewById(R.id.forecastBTTime)
+            private val FBTTemp : TextView = itemView.findViewById(R.id.forecastBTTemp)
+            private val FBTImg : ImageView = itemView.findViewById(R.id.forecastBTImg)
+
+            fun bind(index: Int, list: List<FBT>) {
+                FBTImg.setImageResource(list[index].img)
+                FBTTemp.text = list[index].temp
+                FBTTime.text = list[index].time
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FBTViewHolder {
+        val context : Context = parent.context
+
+        val layoutIdForFBTItem : Int = R.layout.forecast_by_time_item
+
+        val inflater : LayoutInflater = LayoutInflater.from(context)
+
+        val view : View = inflater.inflate(layoutIdForFBTItem, parent, false)
+
+        return FBTViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return FBTNumber
+    }
+
+    override fun onBindViewHolder(holder: FBTViewHolder, position: Int) {
+        holder.bind(position, FBTList)
+    }
+}
--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val FBTRecycler: RecyclerView = findViewById(R.id.recyclerViewFBT)
+
+        // значения заглушек
+        val FBTList : List<FBT> = listOf(
+            FBT("12:00", "2", R.drawable.cloud),
+            FBT("14:00", "5", R.drawable.sun),
+            FBT("16:00", "0", R.drawable.snow),
+            FBT("18:00", "0", R.drawable.snow),
+            FBT("20:00", "3", R.drawable.cloud),
+            FBT("22:00", "1", R.drawable.snow),
+            FBT("00:00", "4", R.drawable.snow)
+        )
+
+        val FBTAdapter = FBTAdapter(FBTList.size, FBTList)
+
+        val layoutManager = LinearLayoutManager(
+            this, LinearLayoutManager.HORIZONTAL, false
+        )
+
+        FBTRecycler.layoutManager = layoutManager
+        FBTRecycler.adapter = FBTAdapter
     }
 }
