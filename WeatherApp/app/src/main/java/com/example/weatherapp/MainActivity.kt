faa6d267923e0147596091278f9055e2dcd114cb
David Dobryakov <davidobryakov@gmail.com>
2020-12-11 19:07:32 +0300

    [hard] Add placeholder for theme changing

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -53,5 +54,9 @@ class MainActivity : AppCompatActivity() {
 
         FBDRecycler.layoutManager = FBDlayoutManager
         FBDRecycler.adapter = FBDAdapter
+    }
+
+    fun changeTheme (view: View) {
+        setTheme(R.style.BlackTheme)
     }
 }
