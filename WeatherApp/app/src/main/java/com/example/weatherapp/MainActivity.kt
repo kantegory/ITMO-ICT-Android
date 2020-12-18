80abb681e55d36f2d42b951d7fe2affd91055903
David Dobryakov <davidobryakov@gmail.com>
2020-12-18 18:32:05 +0300

    [easy] 7 lab: Add default url, add OpenWeatherMapService

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -5,8 +5,19 @@ import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.http.GET
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val URL = "https://api.openweathermap.org/data/2.5/"
+    }
+
+    interface OpenWeatherMapService {
+        @GET("onecall?lat=59.937500&lon=30.308611&exclude=minutely,hourly,alerts&units=metric&appid=508af3c89d5fdcb4b9f030cea26e964e")
+        fun listWeather(): Call<WeatherResponse>
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
