c1545ee50d6b1ce3f84019bbe312112094607b46
David Dobryakov <davidobryakov@gmail.com>
2020-12-18 18:31:29 +0300

    [easy] 7 lab: add moshi utils file with data classes

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MoshiUtils.kt
@@ -0,0 +1,29 @@
+package com.example.weatherapp
+
+import com.squareup.moshi.Json
+
+class WeatherResponse(
+    @field:Json(name = "timezone") var timezone: String,
+    @field:Json(name = "current") var current: Current,
+    @field:Json(name = "daily") var daily: List<Daily>
+)
+
+class Current(
+    @field:Json(name = "humidity") var humidity: Int,
+    @field:Json(name = "temp") var temp: Float,
+    @field:Json(name = "weather") var weather: List<WeatherDescription>
+)
+
+class Daily(
+    @field:Json(name = "temp") var temp: Temp,
+    @field:Json(name = "weather") var weather: List<WeatherDescription>
+)
+
+class Temp(
+    @field:Json(name = "day") var day: Float
+)
+
+class WeatherDescription(
+    @field:Json(name = "main") var main: String,
+    @field:Json(name = "icon") var icon: String
+)
